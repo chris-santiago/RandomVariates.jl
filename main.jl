bd4f3cd8b75736d01b03ee32a784db7082d1ac83
chris-santiago <cjsantiago@gatech.edu>
2022-03-02 10:20:18 -0500

    fix func sigs

--- main.jl
+++ main.jl
@@ -1,11 +1,14 @@
 using RandomVariates
 using Statistics
 using Distributions
+using Gadfly
+import Cairo, Fontconfig
 
 
-x = RandomVariates.normal_rng(0,1,10000)
+x = RandomVariates.normal_rng(0, 1, 10000)
 Statistics.mean(x)
 Statistics.std(x)
+plot(x=x, Geom.histogram(density=true))
 
 u = RandomVariates.binomial_rng(.2342, 1000, 1000)
 sum(u)/1000
@@ -14,4 +17,6 @@ d = Distributions.Binomial(1000, .2342)
 z = rand(d, 1000)
 sum(z)/1000
 
-e = RandomVariates.expon_rng(λ=1, size=10)
+e = RandomVariates.expon_rng(1, 10000)
+exp_plot = plot(x=e, Geom.histogram(density=true))
+exp_plot |> PNG("expon.png", 4inch, 4inch)
