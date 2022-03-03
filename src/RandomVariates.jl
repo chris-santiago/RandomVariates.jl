99624036c5805be644038447a54c597140de7437
chris-santiago <cjsantiago@gatech.edu>
2022-03-02 23:01:05 -0500

    add docstrings; generalize rngs to n-dims

--- src/RandomVariates.jl
+++ src/RandomVariates.jl
@@ -62,7 +62,36 @@ function gen_prn()
 end
 
 
-function get_std_uniform(size=1; seed=nothing)
+"""
+    get_std_normal(size=1, seed=nothing)
+
+Generate a `size` element array of random variables from a Uniform(0,1) distribution.
+
+# Examples
+
+```julia-repl
+julia> get_std_uniform()
+1-element Vector{Float64}:
+ 0.42443098343863284
+```
+
+```julia-repl
+julia> get_std_uniform(seed=43)
+1-element Vector{Float64}:
+ 0.09636209187468836
+```
+
+```julia-repl
+julia> get_std_uniform(5)
+5-element Vector{Float64}:
+ 0.6584669595802204
+ 0.33437978955868886
+ 0.509019330923099
+ 0.12156905126458639
+ 0.917393216014684
+```
+"""
+function get_std_uniform(size::Union{Int, NTuple{3, Int}}=1; seed::Union{Int, Nothing}=nothing)
     if !isnothing(seed)
         set_user_seed(seed)
     end
@@ -73,53 +102,124 @@ function get_std_uniform(size=1; seed=nothing)
 end
 
 
-function uniform_rng(a, b, size=1; seed=nothing)
+"""
+    uniform_rng(a, b, size=1, seed=nothing)
+
+Generate a `size` element array of random variables from a Uniform(`a`, `b`) distribution.
+
+# Examples
+
+```julia-repl
+julia> uniform_rng(1, 6)
+1-element Vector{Float64}:
+ 2.638331960912094
+```
+
+```julia-repl
+julia> uniform_rng(1, 6, seed=42)
+1-element Vector{Float64}:
+ 2.6333962626438314
+```
+
+```julia-repl
+julia> uniform_rng(0, 1, (4,4))
+4×4 Matrix{Float64}:
+ 0.640603   0.757195  0.325722  0.645452
+ 0.955188   0.155203  0.953206  0.0046541
+ 0.0923526  0.490721  0.451705  0.516445
+ 0.661619   0.527063  0.212847  0.832298
+ ```
+"""
+function uniform_rng(a::Real=0, b::Real=1, size::Union{Int, NTuple{3, Int}}=1; seed::Union{Int, Nothing}=nothing)
     U = get_std_uniform(size, seed=seed)
     X = a .+ (b-a) .* U
     return X
 end
 
 
-function expon_rng(λ, size=1; seed=nothing)
+"""
+    expon_rng(λ, size=1, seed=nothing)
+
+Generate a `size` element array of random variables from a Exponential(`λ`) distribution.
+
+# Examples
+
+```julia-repl
+julia> expon_rng(3)
+1-element Vector{Float64}:
+ 0.07033135663980515
+```
+
+```julia-repl
+julia> expon_rng(1.2, seed=42)
+1-element Vector{Float64}:
+ 0.3296112244200808
+```
+
+```julia-repl
+julia> expon_rng(1.2, (2, 2))
+2×2 Matrix{Float64}:
+ 1.9327    0.134739
+ 0.746861  0.155614
+```
+"""
+function expon_rng(λ::Real, size::Union{Int, NTuple{3, Int}}=1; seed::Union{Int, Nothing}=nothing)
     U = get_std_uniform(size, seed=seed)
     X = (-1/λ) .* log.(1 .- U)  # could also use just U
     return X
 end
 
 
-function erlang_rng(k, λ, size=1; seed=nothing)
-    U = zeros(k, size)
-    for i in 1:k
-        U[i, :] = get_std_uniform(size, seed=seed)
-    end
-    # X = (-λ/k) .* log.(prod(U, dims=1))
-    X = (-1/λ) .* log.(prod(U, dims=1))  # Here (-1/λ) represents mean
+"""
+    erlang_rng(k, λ, size=1, seed=nothing)
+
+Generate a `size` element array of random variables from a Erlang_{`k`}(`λ`) distribution.
+
+# Examples
+
+```julia-repl
+julia> erlang_rng(5, .5)
+1-element Vector{Float64}:
+ 10.803989701023117
+```
+
+```julia-repl
+julia> erlang_rng(3, 1, (2,2))
+2×2×1 Array{Float64, 3}:
+[:, :, 1] =
+ 2.19956  4.18505
+ 5.46892  2.5633
+```
+"""
+function erlang_rng(k::Int, λ::Real, size::Union{Int, NTuple{2, Int}}=1; seed::Union{Int, Nothing}=nothing)
+    U = get_std_uniform((size..., k), seed=seed)
+    X = (-1/λ) .* log.(prod(U, dims=3))  # Here (-1/λ) represents mean
     return X
 end
 
 
-function weibull_rng(λ, β, size=1; seed=nothing)
+function weibull_rng(λ::Real, β::Real, size::Int=1; seed::Union{Int, Nothing}=nothing)
     U = get_std_uniform(size, seed=seed)
     X = (1/λ) .* (-log.(1 .- U)) .^ (1/β)
     return X
 end
 
 
-function bernoulli_rng(p, size=1; seed=nothing)
+function bernoulli_rng(p::Float, size::Int=1; seed::Union{Int, Nothing}=nothing)
     U = get_std_uniform(size, seed=seed)
     X = (1 - p) .<= U
     return X
 end
 
 
-function geometric_rng(p, size=1; seed=nothing)
+function geometric_rng(p::Float, size::Int=1; seed::Union{Int, Nothing}=nothing)
     U = get_std_uniform(size, seed=seed)
     X = ceil.(Int, log.(1 .- U) ./ log(1 - p))
     return X
 end
 
 
-function binomial_rng(p, n, size=1; seed=nothing)
+function binomial_rng(p::Float, n::Int, size::Int=1; seed::Union{Int, Nothing}=nothing)
     U = zeros(Int, size, n)
     for i in 1:size
         U[i, :] = bernoulli_rng(p, n, seed=seed)
@@ -129,7 +229,7 @@ function binomial_rng(p, n, size=1; seed=nothing)
 end
 
 
-function poisson_rng(λ, size=1; seed=nothing)
+function poisson_rng(λ::Real, size::Int=1; seed::Union{Int, Nothing}=nothing)
     X = zeros(Int, size)
     for i in 1:size
         X[i] = sum(cumsum(expon_rng(λ, λ*1e2, seed=seed)) .< 1)
@@ -138,7 +238,7 @@ function poisson_rng(λ, size=1; seed=nothing)
 end
 
 
-function get_std_normal(size=1; seed=nothing)
+function get_std_normal(size::Int=1; seed::Union{Int, Nothing}=nothing)
     a = sqrt.(-2 .* log.(get_std_uniform(size, seed=seed)))
     b = 2 * π .* get_std_uniform(size, seed=seed)
     A = a .* sin.(b)
@@ -148,13 +248,13 @@ function get_std_normal(size=1; seed=nothing)
 end
 
 
-function normal_rng(μ=0, σ²=1, size=1; seed=nothing)
+function normal_rng(μ::Real=0, σ²::Real=1, size::Int=1; seed::Union{Int, Nothing}=nothing)
     X = get_std_normal(size, seed=seed) .* σ² .+ μ
     return X
 end
 
 
-function get_gamma_prn(α, β=1; seed=nothing)  # slow
+function get_gamma_prn(α::Real, β=1; seed::Union{Int, Nothing}=nothing)
     if α < 1
         u = get_std_uniform(seed=seed)[1]
         x = get_gamma_prn(α+1, β)
@@ -180,7 +280,7 @@ function get_gamma_prn(α, β=1; seed=nothing)  # slow
 end
 
 
-function gamma_rng(α, β=1, size=1; seed=nothing)
+function gamma_rng(α::Real, β=1, size::Int=1; seed::Union{Int, Nothing}=nothing)
     X = zeros(size)
     X .= get_gamma_prn.(α, β, seed=seed)
     return X
