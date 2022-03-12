1ba33dd5eea2d8cbf365c04107006ee4113795f8
chris-santiago <cjsantiago@gatech.edu>
2022-03-11 20:54:47 -0500

    remove old gamma prn method

--- src/gamma.jl
+++ src/gamma.jl
@@ -1,45 +1,4 @@
 """
-    get_gamma_prn(α, β; seed=nothing)
-
-Generate a random variable from a Gamma(`α`, `β`) distribution. Optionally you can set a specific seed.
-
-# Notes
-
-This method cannot handle values of α < 1. Users should use [`gamma_rng`](@ref), instead.
-
-# Examples
-
-```julia-repl
-julia> get_gamma_prn(1)
- 0.11697493392617847
-
-julia> get_gamma_prn(1, 2)
- 0.09261426424752814
-```
-
-# References
-
-D.P. Kroese, T. Taimre, Z.I. Botev. Handbook of Monte Carlo Methods. 
-  Wiley Series in Probability and Statistics, John Wiley & Sons, New York, 2011.
-"""
-function get_gamma_prn(α::Real, β::Real=1; seed::Union{Int, Nothing}=nothing)
-    seed_setter(seed)
-    d = α - (1/3)
-    c = 1 / sqrt(9*d)
-    while true
-        z = get_std_normal()[1]
-        if z > -1/c
-            v = (1 + c * z) ^ 3
-            u = get_std_uniform()[1]
-            if log(u) <= (0.5 * z^2 + d - (d * v + d * log(v)))
-                return d * v / β
-            end
-        end
-    end
-end
-
-
-"""
     gamma_rng(α, β, shape=1; seed=nothing)
 
 Generate a `shape` element array of random variables from a Gamma(`α`, `β`) distribution. Optionally you can set a specific seed.
